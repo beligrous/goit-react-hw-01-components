150aca4e18e6271873cdff08a1273168d6637d35
beligrous <beligrous@gmail.com>
2023-01-07 19:37:34 +0200

    1st task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import user from '../user.json';
+import { Profile } from './profile';
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        avatar={user.avatar}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,41 +1,27 @@
-import user from './';
-
+import user from '../user.json';
 export function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div class="profile">
       <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <img src={user.avatar} alt={user.username} class="avatar" />
+        <p class="name">{user.username}</p>
+        <p class="tag">{user.tag}</p>
+        <p class="location">{user.location}</p>
       </div>
-
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{user.stats.followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{user.stats.views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{user.stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
-
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>;
